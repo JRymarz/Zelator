99e5064a502235f1803883ab0677836761421b88
JRymarz <jakubrymarz1@gmail.com>
2024-10-22 20:27:02 +0200

    Initial commit

---
+++ src/main/java/org/zelator/ZelatorApplication.java
@@ -0,0 +1,13 @@
+package org.zelator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZelatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZelatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zelator/ZelatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zelator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZelatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
